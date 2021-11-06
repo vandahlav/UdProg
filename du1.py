9844c8a5381204fa95a9dfc00fce1bcfa76eb6d8
vandahlav <93780877+vandahlav@users.noreply.github.com>
2021-11-06 15:45:04 +0100

    Add files via upload

--- du1.py
+++ du1.py
@@ -57,31 +57,30 @@ cross_player = True
 for _ in range (pocet_policek):    
     #první hraje X hráč
     if cross_player == True:
-        Xhrac_x = int(input("X hráči, zadej x-ovou souřadnici: ")) 
-        while Xhrac_x > radek or Xhrac_x < 0:
-            Xhrac_x = int(input("Chybný zápis, zadejte znovu souřadnice pro X: "))
-        Xhrac_y = int(input("X hráči, zadej y-ovou souřadnici: "))
-        while Xhrac_y > sloupec or Xhrac_y < 0:
-            Xhrac_y = int(input("Chybný zápis, zadejte znovu souřadnice pro Y: ")) 
-        t.setpos(Xhrac_x*delka_strany-delka_strany,Xhrac_y*delka_strany-delka_strany)
+        hrac_x = int(input("X hráči, zadej x-ovou souřadnici: ")) 
+        while hrac_x > radek or hrac_x < 0:
+            hrac_x = int(input("Chybný zápis, zadejte znovu souřadnice pro X: "))
+        hrac_y = int(input("X hráči, zadej y-ovou souřadnici: "))
+        while hrac_y > sloupec or hrac_y < 0:
+            hrac_y = int(input("Chybný zápis, zadejte znovu souřadnice pro Y: ")) 
+        t.setpos(hrac_x*delka_strany-delka_strany,hrac_y*delka_strany-delka_strany)
         t.pendown()
         draw_cross(sqrt(2*(delka_strany**2)))
         t.right(135)
         t.penup()
     else:
-        Ohrac_x = int(input("O hráči, zadej x-ovou souřadnici: "))  
-        while Ohrac_x > radek or Ohrac_x < 0:
-            Ohrac_x = int(input("Chybný zápis, zadejte znovu souřadnice pro X: "))
-        Ohrac_y = int(input("O hráči, zadej y-ovou souřadnici: "))     
-        while Ohrac_y > sloupec or Ohrac_y < 0:
-            Ohrac_y = int(input("Chybný zápis, zadejte znovu souřadnice pro Y: "))
-        Ohrac_x = Ohrac_x*delka_strany
-        Ohrac_y = Ohrac_y*delka_strany
-        t.setpos(Ohrac_x-(delka_strany/2),Ohrac_y-delka_strany)
+        hrac_x = int(input("O hráči, zadej x-ovou souřadnici: "))  
+        while hrac_x > radek or hrac_x < 0:
+            hrac_x = int(input("Chybný zápis, zadejte znovu souřadnice pro X: "))
+        hrac_y = int(input("O hráči, zadej y-ovou souřadnici: "))     
+        while hrac_y > sloupec or hrac_y < 0:
+            hrac_y = int(input("Chybný zápis, zadejte znovu souřadnice pro Y: "))
+        hrac_x = hrac_x*delka_strany
+        hrac_y = hrac_y*delka_strany
+        t.setpos(hrac_x-(delka_strany/2),hrac_y-delka_strany)
         t.pendown()
         draw_circle(delka_strany/2)
         t.penup()
-    cross_player = not cross_player     #dochází zde k negaci původní hodnoty cross_player a while cyklus tak střídá mezi oběma hráči 
-    pocet_policek = pocet_policek - 1   #odečtením se snižuje počet tahů ve hře
+    cross_player = not cross_player     #dochází zde k negaci původní hodnoty cross_player a for cyklus tak střídá mezi oběma hráči 
 print("GAME OVER!")
 turtle.exitonclick()
